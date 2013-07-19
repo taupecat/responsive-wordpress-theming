2dc7dd70e7aff66d4cfda73601a1e64ec0cbfd6b
Tracy Rotton <trotton@rp3agency.com>
2013-07-19 09:24:38 -0400

    Using Sass' "force preserve comments" feature to go back to using style.css as the compiled CSS file name; Changed the "base" directory to "underscores" to make it clearer that those styles came from the main _s project; Updated the component underscores pieces to match the latest code from Automattic

--- chapter-06/functions.php
+++ chapter-06/functions.php
@@ -83,7 +83,7 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles
  */
 function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_stylesheet_directory_uri() . '/stylesheets/underscores.css');
+	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
