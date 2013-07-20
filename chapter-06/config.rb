80e19c40b6d15a4e2edc15a4aa4c024c14141dbc
Tracy Rotton <tracy@taupecat.com>
2013-07-20 00:52:53 -0400

    Turned on full Sass debugging info. Be sure to turn this off and uncomment "line_comments = false" before compiling your CSS for production!

--- chapter-06/config.rb
+++ chapter-06/config.rb
@@ -22,3 +22,4 @@ javascripts_dir = "js"
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+sass_options = {:debug_info => true}
