8935b3ae89b0691f9273ac1bec454987e1014b79
Tracy Rotton <tracy@taupecat.com>
2013-08-21 00:15:31 -0400

    Restored files that somehow vanished.

---
+++ chapter-07/manning/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Manning
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'manning_credits' ); ?>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'manning' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'manning' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'manning' ), 'manning', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ chapter-07/manning/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * manning Theme Customizer
+ *
+ * @package Manning
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function manning_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'manning_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function manning_customize_preview_js() {
+	wp_enqueue_script( 'manning_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'manning_customize_preview_js' );
