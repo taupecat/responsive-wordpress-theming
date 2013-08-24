46b4426ac773b557b64ed94199be03f555a32689
Tracy Rotton <tracy@taupecat.com>
2013-08-24 15:46:28 -0400

    converted tabs to spaces for better legibility in the book listings

--- chapter-07/manning/functions.php
+++ chapter-07/manning/functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 640; /* pixels */
+  $content_width = 640; /* pixels */
 
 if ( ! function_exists( 'manning_setup' ) ) :
 /**
@@ -21,45 +21,45 @@ if ( ! function_exists( 'manning_setup' ) ) :
  */
 function manning_setup() {
 
-	/**
-	 * Make theme available for translation
-	 * Translations can be filed in the /languages/ directory
-	 * If you're building a theme based on manning, use a find and replace
-	 * to change 'manning' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'manning', get_template_directory() . '/languages' );
+  /**
+   * Make theme available for translation
+   * Translations can be filed in the /languages/ directory
+   * If you're building a theme based on manning, use a find and replace
+   * to change 'manning' to the name of your theme in all the template files
+   */
+  load_theme_textdomain( 'manning', get_template_directory() . '/languages' );
 
-	/**
-	 * Add default posts and comments RSS feed links to head
-	 */
-	add_theme_support( 'automatic-feed-links' );
+  /**
+   * Add default posts and comments RSS feed links to head
+   */
+  add_theme_support( 'automatic-feed-links' );
 
-	/**
-	 * Enable support for Post Thumbnails on posts and pages
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	//add_theme_support( 'post-thumbnails' );
+  /**
+   * Enable support for Post Thumbnails on posts and pages
+   *
+   * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+   */
+  //add_theme_support( 'post-thumbnails' );
 
-	/**
-	 * This theme uses wp_nav_menu() in one location.
-	 */
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'manning' ),
-	) );
+  /**
+   * This theme uses wp_nav_menu() in one location.
+   */
+  register_nav_menus( array(
+    'primary' => __( 'Primary Menu', 'manning' ),
+  ) );
 
-	/**
-	 * Enable support for Post Formats
-	 */
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+  /**
+   * Enable support for Post Formats
+   */
+  add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
 
-	/**
-	 * Setup the WordPress core custom background feature.
-	 */
-	add_theme_support( 'custom-background', apply_filters( 'manning_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+  /**
+   * Setup the WordPress core custom background feature.
+   */
+  add_theme_support( 'custom-background', apply_filters( 'manning_custom_background_args', array(
+    'default-color' => 'ffffff',
+    'default-image' => '',
+  ) ) );
 }
 endif; // manning_setup
 add_action( 'after_setup_theme', 'manning_setup' );
@@ -68,14 +68,14 @@ add_action( 'after_setup_theme', 'manning_setup' );
  * Register widgetized area and update sidebar with default widgets
  */
 function manning_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'manning' ),
-		'id'            => 'sidebar-1',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+  register_sidebar( array(
+    'name'          => __( 'Sidebar', 'manning' ),
+    'id'            => 'sidebar-1',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ) );
 }
 add_action( 'widgets_init', 'manning_widgets_init' );
 
@@ -83,19 +83,19 @@ add_action( 'widgets_init', 'manning_widgets_init' );
  * Enqueue scripts and styles
  */
 function manning_scripts() {
-	wp_enqueue_style( 'manning-style', get_stylesheet_uri() );
+  wp_enqueue_style( 'manning-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'manning-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+  wp_enqueue_script( 'manning-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'manning-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+  wp_enqueue_script( 'manning-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+    wp_enqueue_script( 'comment-reply' );
+  }
 
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'manning-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
+  if ( is_singular() && wp_attachment_is_image() ) {
+    wp_enqueue_script( 'manning-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+  }
 }
 add_action( 'wp_enqueue_scripts', 'manning_scripts' );
 
