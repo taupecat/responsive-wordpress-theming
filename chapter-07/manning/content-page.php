212fc656196950e18233d3ef0b216eb6a54cda31
Tracy Rotton <tracy@taupecat.com>
2013-08-24 09:59:13 -0400

    Adjusting spacing to appear better in the book.

--- chapter-07/manning/content-page.php
+++ chapter-07/manning/content-page.php
@@ -7,18 +7,18 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
+  <header class="entry-header">
+    <h1 class="entry-title"><?php the_title(); ?></h1>
+  </header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'manning' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-	<?php edit_post_link( __( 'Edit', 'manning' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+  <div class="entry-content">
+    <?php the_content(); ?>
+    <?php
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . __( 'Pages:', 'manning' ),
+        'after'  => '</div>',
+      ) );
+    ?>
+  </div><!-- .entry-content -->
+  <?php edit_post_link( __( 'Edit', 'manning' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
 </article><!-- #post-## -->
