731b9573f1b2351708199b0cd3ba9623c4916735
Tracy Rotton <tracy@taupecat.com>
2013-08-24 20:48:10 -0400

    Converted tabs to 2 spaces for better legibility in book

--- chapter-07/manning/footer.php
+++ chapter-07/manning/footer.php
@@ -8,16 +8,16 @@
  */
 ?>
 
-	</div><!-- #content -->
+  </div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'manning_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'manning' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'manning' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'manning' ), 'manning', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+  <footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+      <?php do_action( 'manning_credits' ); ?>
+      <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'manning' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'manning' ), 'WordPress' ); ?></a>
+      <span class="sep"> | </span>
+      <?php printf( __( 'Theme: %1$s by %2$s.', 'manning' ), 'manning', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+    </div><!-- .site-info -->
+  </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
