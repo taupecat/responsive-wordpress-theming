ec3702becd78e071a0ee20d137fe6d73ef4fdca4
Tracy Rotton <tracy@taupecat.com>
2013-08-29 00:04:51 -0400

    Added whitespace for better legibility

--- chapter-07/manning/content-page.php
+++ chapter-07/manning/content-page.php
@@ -7,11 +7,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
   <header class="entry-header">
     <h1 class="entry-title"><?php the_title(); ?></h1>
   </header><!-- .entry-header -->
 
   <div class="entry-content">
+
     <?php the_content(); ?>
     <?php
       wp_link_pages( array(
@@ -19,6 +21,11 @@
         'after'  => '</div>',
       ) );
     ?>
-  </div><!-- .entry-content -->
+
+  </div>
+  <!-- .entry-content -->
+
   <?php edit_post_link( __( 'Edit', 'manning' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
-</article><!-- #post-## -->
+
+</article>
+<!-- #post-## -->
