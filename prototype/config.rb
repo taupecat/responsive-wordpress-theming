f02989a9154d61c44968be26c773e29ef95f4dc0
Tracy Rotton <tracy@taupecat.com>
2013-09-02 17:33:13 -0400

    Renamed "scss" to "sass" to better match how I have my Sass-enabled _s fork set up

--- prototype/config.rb
+++ prototype/config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = ""
-sass_dir = "scss"
+sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
 fonts_dir = "fonts"
