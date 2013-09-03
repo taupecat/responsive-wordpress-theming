ba303e3ceeee2644c6e3fe3a8b24fc48d905c6ff
Tracy Rotton <tracy@taupecat.com>
2013-09-02 23:02:49 -0400

    converted tabs to spaces

--- chapter-07/manning/header.php
+++ chapter-07/manning/header.php
@@ -10,7 +10,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
@@ -19,20 +19,57 @@
 </head>
 
 <body <?php body_class(); ?>>
+
 <div id="page" class="hfeed site">
-	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'manning' ); ?></h1>
-			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'manning' ); ?>"><?php _e( 'Skip to content', 'manning' ); ?></a></div>
+  <?php do_action( 'before' ); ?>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+  <!-- Begin Site Header -->
 
-	<div id="content" class="site-content">
+  <header id="masthead" class="site-header" role="banner">
+
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+
+      <img src="http://placehold.it/123x123&amp;text=[logo]" class="logo" width="123" height="123">
+
+      <div class="site-branding">
+        <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+        <div class="site-description"><?php bloginfo( 'description' ); ?></div>
+      </div>
+      <!-- // .site-branding -->
+
+    </a>
+    <!-- // [rel="home"] -->
+
+    <!-- Begin Navigation -->
+
+    <div class="navbar">
+
+      <a href="#content" class="screen-reader-text skip-link"><?php _e( 'Skip to content', 'manning' ); ?></a>
+
+      <nav id="site-navigation" class="primary-navigation" role="navigation">
+
+        <a href="#!" id="toggle-primary-nav" class="menu-toggle"><?php _e( 'Menu', 'manning' ); ?></a>
+
+        <?php wp_nav_menu( array( 
+          'theme_location'  => 'primary',
+          'container_class' => 'primary-navigation-container menu',
+          'container_id'    => 'primary-navigation-container',
+        ) ); ?>
+
+      </nav>
+      <!-- #site-navigation -->
+
+      <!-- End Primary Navigation -->
+
+    </div>
+    <!-- // .navbar -->
+
+    <!-- End Navigation -->
+
+  </header>
+  <!-- #masthead -->
+
+  <!-- End Site Header -->
+
+  <div id="content" class="site-content">
