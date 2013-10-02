3bd6922d232da11641c3c307edfc00aad9eb6de0
Tracy Rotton <tracy@taupecat.com>
2013-10-02 10:55:34 -0400

    Only display a logo if one has been provided (no default logo)

--- manning/header.php
+++ manning/header.php
@@ -30,7 +30,9 @@
 
     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 
-      <img src="http://placehold.it/123x123&amp;text=[logo]" class="logo" width="123" height="123">
+      <?php if ( get_option( 'logo' ) ): ?>
+      <img src="<?php echo get_option( 'logo' ); ?>" alt="" class="logo">
+      <?php endif; ?>
 
       <div class="site-branding">
         <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
